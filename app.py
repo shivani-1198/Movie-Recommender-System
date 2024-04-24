e3a788fd4e4e664ae58999efee44b8e45e9acd26
Shivani Agarwal <77854814+shivani-1198@users.noreply.github.com>
2024-04-24 19:34:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,24 +47,7 @@ selected_movie_name = st.selectbox(
 # creating a button which when clicked our website will return 5 most common movies
 if st.button("Recommend"):
     names , posters = recommend(selected_movie_name) # calling the function recomend to get the movies
-
-    # col1, col2, col3, col4, col5 = st.columns(5)
-
-    # with col1:
-    #     st.text(names[0])
-    #     st.image(posters[0])
-    # with col2:
-    #     st.text(names[1])
-    #     st.image(posters[1])
-    # with col3:
-    #     st.text(names[2])
-    #     st.image(posters[2])
-    # with col4:
-    #     st.text(names[3])
-    #     st.image(posters[3])
-    # with col5:
-    #     st.text(names[4])
-    #     st.image(posters[4])
+    
     num_cols = 5  # Number of columns to display
     cols = st.columns(num_cols)
 
